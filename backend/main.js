642f4e54ac14af36c9ecf0da69b4af12cc108f6d
9-ohm <5910110262@email.psu.ac.th>
2022-04-19 11:30:20 +0700

    dbcon

--- backend/main.js
+++ backend/main.js
@@ -10,7 +10,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
+let dbCon = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database:  'helpdesk'
+})
+dbCon.connect();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
