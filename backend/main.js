15b68ffeddf8494c8932533cfac0b783bb9b5c94
9-ohm <5910110262@email.psu.ac.th>
2022-04-20 10:04:49 +0700

    try comm

--- backend/main.js
+++ backend/main.js
@@ -18,6 +18,21 @@ let dbCon = mysql.createConnection({
 })
 dbCon.connect();
 
+app.get('/staffs',(req, res) => {
+  dbCon.query('SELECT * FROM staffs',(error, results, fields)=>{
+    if(error) throw error
+
+    let massages =""
+    if(results === undefined || results.length == 0){
+      logger.warn('Staff table is empty');
+    }else {
+      logger.info('Successful get all staff');
+    }
+    
+    res.send({error: false, data: results})
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
