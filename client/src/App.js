b2ff9358f473cad9d6762da3e021ddc2461ab76e
9-ohm <5910110262@email.psu.ac.th>
2022-04-20 14:04:14 +0700

    form

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,28 @@ function App() {
       <div className="information">
         <from action="">
           <div className="mb-3">
-            <label htmlFor="ชื่อครุภัณฑ์" className="form-label">Name:</label>
+            <label htmlFor="ชื่อครุภัณฑ์" className="form-label">ชื่อครุภัณฑ์:</label>
             <input type="text" className="form-control" placeholder="ระบุชื่อครุภัณฑ์"/>
           </div>
           <div className="mb-3">
-            <label htmlFor="หมายเลขครุภัณฑ์" className="form-label">Name:</label>
+            <label htmlFor="หมายเลขครุภัณฑ์" className="form-label">หมายเลขครุภัณฑ์:</label>
             <input type="number" className="form-control" placeholder="ระบุหมายเลขครุภัณฑ์"/>
           </div>
           <div className="mb-3">
-            <label htmlFor="ตำแหน่ง" className="form-label">Name:</label>
-            <input type="text" className="form-control" placeholder="ระบุตำแหน่งอุปกรณ์"/>
+            <label htmlFor="ตำแหน่งที่ตั้งอุปกรณ์" className="form-label">ตำแหน่งที่ตั้งอุปกรณ์:</label>
+            <input type="text" className="form-control" placeholder="ระบุตำแหน่งที่ตั้งอุปกรณ์"/>
           </div>
           <div className="mb-3">
-            <label htmlFor="อาการเสีย" className="form-label">Name:</label>
+            <label htmlFor="อาการเสีย" className="form-label">อาการเสีย:</label>
             <input type="text" className="form-control" placeholder="ระบุอาการเสีย"/>
           </div>
+          <button className="btn btn-success">ยืนยัน</button>
         </from>
       </div>
-      
+      <hr/>
+      <div className="articles">
+        <button className="btn btn-primary">แสดงรายการแจ้งซ่อม</button>
+      </div>
     </div>
   );
 }
