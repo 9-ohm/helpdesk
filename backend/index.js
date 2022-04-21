f78013aa59453b282954366fa0a94541f08f9062
9-ohm <5910110262@email.psu.ac.th>
2022-04-21 10:59:25 +0700

    selector

---
+++ backend/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+const { createLogger, format, transports } = require('winston');
+
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+const port = 3001
+
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, 'results.log');
+
+const logger = createLogger({
+    // change level if in dev environment versus production
+    level: env === 'development' ? 'debug' : 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new transports.Console({
+            level: 'info',
+            format: format.combine(
+                format.colorize(),
+                format.printf(
+                    info => `${info.timestamp} ${info.level}: ${info.message}`
+                )
+            )
+        }),
+        new transports.File({ filename })
+    ]
+});
+
+
+app.use(cors())
+app.use(express.json())
+
+let db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'helpdesk'
+})
+
+app.get('/articles', (req, res) => {
+    db.query('SELECT * FROM durablearticles', (error, results, fields) => {
+        if (error) throw error
+        if (results === undefined || results.length == 0) {
+            logger.warn('Table is empty');
+        } else {
+            logger.info('Successful get all articles');
+            res.send(results)
+        }
+
+    })
+})
+
+app.post('/addarticles', (req, res) => {
+    let name = req.body.name
+    let id = req.body.id
+    let location = req.body.location
+    let problem = req.body.problem
+
+    db.query(
+        "INSERT INTO durablearticles (name, id, location, problem) VALUES(?,?,?,?)",
+        [name, id, location,problem], (error, results,) => {
+            if (error) throw error;
+            else {
+                logger.info("sussessfuly added");
+                return res.send("successfully added")
+            }
+        })
+})
+
+app.listen(port, () => {
+    logger.info('Server is running on port ' + port)
+})
--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,117 @@
+import Axios from 'axios';
+import { useState } from 'react'
+import { Form } from "react-bootstrap";
+
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [id, setId] = useState(0)
+  const [location, setLocation] = useState("")
+  const [condition, setCondition] = useState("")
+  const [type, setType] = useState("Dictamen");
+
+  const [articleList, setArticleList] = useState([])
+
+  const getArticles = () => {
+    Axios.get('http://localhost:3001/articles').then((response) => {
+      setArticleList(response.data)
+    })
+  }
+
+  const addArticles = () => {
+    Axios.post('http://localhost:3001/addarticles', {
+      name: name,
+      id: id,
+      location: location,
+      condition: condition
+    }).then(() => {
+      setArticleList([
+        ...articleList,
+        {
+          name: name,
+          id: id,
+          location: location,
+          condition: condition
+        }
+      ])
+    })
+  }
   return (
     <div className="App container">
-      <h1>ระบบแจ้งซ่อม</h1>
+      <h1>รายละเอียดแจ้งซ่อม</h1>
       <div className="information">
         <from action="">
           <div className="mb-3">
             <label htmlFor="ชื่อครุภัณฑ์" className="form-label">ชื่อครุภัณฑ์:</label>
-            <input type="text" className="form-control" placeholder="ระบุชื่อครุภัณฑ์"/>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ระบุชื่อครุภัณฑ์"
+              onChange={(event) => {
+                setName(event.target.value)
+              }}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="หมายเลขครุภัณฑ์" className="form-label">หมายเลขครุภัณฑ์:</label>
-            <input type="number" className="form-control" placeholder="ระบุหมายเลขครุภัณฑ์"/>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="ระบุหมายเลขครุภัณฑ์"
+              onChange={(event) => {
+                setId(event.target.value)
+              }}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="ตำแหน่งที่ตั้งอุปกรณ์" className="form-label">ตำแหน่งที่ตั้งอุปกรณ์:</label>
-            <input type="text" className="form-control" placeholder="ระบุตำแหน่งที่ตั้งอุปกรณ์"/>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="ระบุตำแหน่งที่ตั้งอุปกรณ์"
+              onChange={(event) => {
+                setLocation(event.target.value)
+              }}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="อาการเสีย" className="form-label">อาการเสีย:</label>
-            <input type="text" className="form-control" placeholder="ระบุอาการเสีย"/>
+            <Form.Group controlId="formBasicSelect">
+              <Form.Control
+                as="select"
+                value={type}
+                onChange={(event) => {
+                  setType(event.target.value);
+                }}
+              >
+                <option selected>โปรดเลือกปัญหาที่พบ</option>
+                <option value="เปิดไม่ติด">เปิดไม่ติด</option>
+                <option value="ใช้งานอินเทอร์เน็ตไม่ได้">ใช้งานอินเทอร์เน็ตไม่ได้</option>
+              </Form.Control>
+            </Form.Group>
+
           </div>
-          <button className="btn btn-success">ยืนยัน</button>
+
+          <button className="btn btn-success" onClick={addArticles}>ยืนยัน</button>
         </from>
       </div>
-      <hr/>
+      <hr />
       <div className="articles">
-        <button className="btn btn-primary">แสดงรายการแจ้งซ่อม</button>
+        <button className="btn btn-primary" onClick={getArticles}>แสดงรายการแจ้งซ่อม</button>
+        <br></br><br></br>
+        {articleList.map((val, key) => {
+          return (
+            <div className='articles card'>
+              <div className='card-body text-left'>
+                <p className="card-text">ชื่อครุภัณฑ์: {val.name}</p>
+                <p className="card-text">หมายเลขครุภัณฑ์: {val.id}</p>
+                <p className="card-text">ตำแหน่งที่ตั้งอุปกรณ์: {val.location}</p>
+                <p className="card-text">อาการเสีย: {val.problem}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
